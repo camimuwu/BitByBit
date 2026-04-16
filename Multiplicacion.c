a2065999f4672c42eb81c49e30b0b3225fbaf72e
yves2100 <mendozamaria0306@gmail.com>
2026-04-16 19:33:32 -0400

    Modificacion en Multiplicacion.c

--- Multiplicacion.c
+++ Multiplicacion.c
@@ -1,7 +1,14 @@
 #include "Multiplicacion.h"
 #include<stdio.h>
 
-void multiplicacion()
-{
-    printf("Hola\n");
+int mult;
+int multiplicacion(int a, int b){
+
+    printf("2");
+    a=2
+    printf("3");
+    b=3;
+    mult=a*b;
+
+    return mult;
 }
