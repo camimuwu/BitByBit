fa3e068ef6433e460d0187df1da564a0eed3979d
yves2100 <mendozamaria0306@gmail.com>
2026-04-17 20:07:25 -0400

    Actualizacion en funcion Suma

--- Suma.c
+++ Suma.c
@@ -1,7 +1,36 @@
 #include "Suma.h"
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-void suma()
-{
-    printf("Hola\n");
+void suma(){
+
+    int min, max, respuesta, correcta;
+    min = 1;
+    max = 10;
+
+    srand(time(NULL));
+
+    for (int i = 0; i<3; i++){
+
+        int a = (rand() % max) + min;
+        int b = (rand() % max) + min;
+
+        printf("Suma a resolver: %d + %d: ", a, b);
+        scanf("%d", &respuesta);
+
+        correcta = a+b;
+
+        if (respuesta == correcta){
+            printf("Correcto\n");
+        }
+        else {
+            printf("Respuesta correcta: %d\n", correcta);
+        }
+
+    }
+    
+
+
+
 }
