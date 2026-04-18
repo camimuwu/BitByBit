8dfbbe402d5fac3b2579c71dd034fe6bf2e747a2
Camila Guerra <camifer110@gmail.com>
2026-04-17 20:58:31 -0400

    Modificación de Resta.c

--- Resta.c
+++ Resta.c
@@ -1,7 +1,33 @@
 #include "Resta.h"
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-void resta()
-{
-    printf("Hola\n");
+void resta(){
+
+    int min, max, respuesta, correcta;
+    min = 1;
+    max = 10;
+
+    srand(time(NULL));
+
+    for (int i = 0; i<3; i++){
+
+        int a = (rand() % max) + min;
+        int b = (rand() % max) + min;
+
+        printf("Resta a resolver: %d - %d: ", a, b);
+        scanf("%d", &respuesta);
+
+        correcta = a-b;
+
+        if (respuesta == correcta){
+            printf("Correcto\n");
+        }
+        else {
+            printf("Respuesta correcta: %d\n", correcta);
+        }
+
+    }
+    
 }
