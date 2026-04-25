43430225c9a3b3519b249695a1f79ef70e89e9a2
yves2100 <mendozamaria0306@gmail.com>
2026-04-24 21:19:23 -0400

    Organizando archivos en carpeta src

---
+++ src/Multiplicacion.c
@@ -0,0 +1,48 @@
+#include "Multiplicacion.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void multiplicacion(){
+
+    int min, max, respuesta, correcta;
+    min = 1;
+    max = 10;
+    int correctas = 0, incorrectas = 0;
+    time_t inicio;
+
+    srand(time(NULL));
+
+    inicio = time(NULL);
+
+    while (difftime(time(NULL), inicio) < 30){
+
+        int a = (rand() % max) + min;
+        int b = (rand() % max) + min;
+
+        printf("Multiplicacion a resolver: %d * %d: ", a, b);
+        scanf("%d", &respuesta);
+
+        correcta = a*b;
+
+        if (respuesta == correcta){
+            printf("Correcto\n");
+            printf("\n");
+            correctas++;
+        }
+        else {
+            printf("Incorrecto, respuesta correcta: %d\n", correcta);
+            printf("\n");
+            incorrectas++;
+        }
+    }
+    printf("El tiempo a acabado!\n");
+    printf("\n");
+    printf("Total de ecuaciones resueltas: %d\n", correctas + incorrectas);
+    printf("Total de respuestas correctas: %d\n", correctas);
+    printf("Total de respuestas incorrectas: %d\n", incorrectas);
+    printf("\n");
+    printf("Precione la tecla enter para volver al menu principal\n");
+    while (getchar() != '\n' && getchar() != EOF);
+    getchar();
+}
